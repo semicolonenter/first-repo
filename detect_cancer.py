edad1d3a9375cb21f501daf9749b5f6f35c02b34
semicolonenter <58291860+semicolonenter@users.noreply.github.com>
2019-12-10 13:52:56 +0100

    this fixes #3

--- detect_cancer.py
+++ detect_cancer.py
@@ -2,3 +2,5 @@ print("OH you've got cancer! you will die in 2 days :(")
 print("I'm just kidding..")
 print("we are going to put you in experimental drugs")
 print("I found the best way to detect cancer! Please use it."
+print(" jpg images")
+      
