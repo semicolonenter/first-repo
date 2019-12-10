c0e3ec591d77432694942e0c8ae65f234cea30eb
nuradhianti <45609235+nuradhianti@users.noreply.github.com>
2019-12-10 13:44:08 +0100

    Update detect_cancer.py

--- detect_cancer.py
+++ detect_cancer.py
@@ -1,3 +1,4 @@
 print("OH you've got cancer! you will die in 2 days :(")
 print("I'm just kidding..")
 print("we are going to put you in experimental drugs")
+print("I found the best way to detect cancer! Please use it."
